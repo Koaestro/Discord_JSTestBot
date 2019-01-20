032297c9ab9a44494cb809b0513459d244657487
Koaestro <koaestro@gmail.com>
2019-01-20 10:01:37 +0000

    Added report command and reports are saved to MongoDB databse

---
+++ Commands/report.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const Report = require("../models/reports.js");
+
+
+
+module.exports = class report {
+
+    constructor(){
+        this.name = "report",
+        this.alias = "r",
+        this.usage = ">report"
+    }
+
+    async run(bot, message, args) {
+
+        await message.delete();
+
+        mongoose.connect('mongodb://localhost/Reports');
+
+        let rUser = message.mentions.members.first();
+        if (!rUser) return message.reply("Couldn't find user");
+        let rreason = args.slice(1).join(" ");
+        if (!rreason) return message.reply("Please include a reason");
+
+        const report = new Report({
+            // _id: mongoose.Types.ObjectID(),
+            username: rUser.user.username,
+            userID: rUser.id,
+            reason: rreason,
+            rUsername: message.author.username,
+            rID: message.author.id,
+            time: message.createdAt
+        });
+
+        report.save()
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+
+        message.reply("Report has been saved");
+
+    }
+}
